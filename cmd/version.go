3c91bd5dd6d024906a49fefd43978c9c3707c063
Lucy Davies <lucy.davies@skybettingandgaming.com>
2018-01-19 16:12:31 +0000

    Remove test faff

--- cmd/version.go
+++ cmd/version.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"runtime"
-
 	log "github.com/Sirupsen/logrus"
 	"github.com/skybet/cali"
 
@@ -15,7 +13,6 @@ func init() {
 	command.SetShort("Which version are we running?")
 
 	var taskFunc cali.TaskFunc = func(t *cali.Task, args []string) {
-		log.Infof("GOOS: %s, GOARCH: %s", runtime.GOOS, runtime.GOARCH)
 		log.Infof("lucli v%s (%s) (%s)", lib.Version, lib.BuildTime, lib.BuildCommit)
 	}
 
