f3675321fa7bd7ff39d3ca36052f2535dd04e76d
Lucy Davies <lucy.davies@skybettingandgaming.com>
2018-04-11 15:40:25 +0100

    Re-order some stuff in cmd/terraform

--- cmd/terraform.go
+++ cmd/terraform.go
@@ -8,12 +8,10 @@ import (
 
 func init() {
 
-	// Default image name/version
-	imageName := "hashicorp/terraform"
-	imageVersion := "latest"
-
 	command := cli.NewCommand("terraform [command]")
+
 	command.SetShort("Run Terraform in an ephemeral container")
+
 	command.SetLong(`Starts a container for Terraform and attempts to run it against your code. There are two choices for code source; a local mount, or directly from a git repo.
 
 Examples:
@@ -25,14 +23,15 @@ Examples:
   # lucli terraform plan -- -state=environments/test/terraform.tfstate -var-file=environments/test/terraform.tfvars
   # lucli terraform -- plan -out plan.out
 `)
+
 	command.Flags().StringP("profile", "p", "default", "Profile to use from the AWS shared credentials file")
 	command.Flags().StringP("terraform-version", "v", imageVersion, "Version of image to use")
 	command.BindFlags()
 
 	// Set default image for Run function
+	imageName := "hashicorp/terraform"
+	imageVersion := "latest"
 	task := command.Task(fmt.Sprintf("%s:%s", imageName, imageVersion))
-
-	// TODO: some amazing witchcraft, with sidekick containers, providing stuff like awscli
 
 	// Init function, set profile, and image version
 	task.SetInitFunc(func(t *cali.Task, args []string) {
